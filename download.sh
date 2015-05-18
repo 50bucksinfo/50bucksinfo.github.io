764a3e1dea4e4bb18a17eaf5e16ebeca51f44853
Mod God <Mod.God@email.com>
2015-05-18 14:33:23 +1000

    Second

---
+++ download.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd ..
+wget      --recursive      --no-clobber      --page-requisites      --html-extension      --convert-links      --restrict-file-names=windows      --domains 50bucks.info      --no-parent          http://50bucks.info
